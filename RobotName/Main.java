31518ee939f20d87a6288f7b81128b9783d1e8b3
Lucas Moidel <lucas122133@gmail.com>
2025-08-13 13:02:58 -0700

    stuff

--- RobotName/Main.java
+++ RobotName/Main.java
@@ -1,30 +1,33 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.sql.Time;
+
 import kareltherobot.*;
 //This is the starter code for Karel the Robot AP CSA Thai
 
 public class Main implements Directions {
   public static void main(String[] args) 
 	{ 
-		//My robot's name is karel
-    Robot l = new Robot(5,1,South,-1);
-    Robot u = new Robot(1,5,East,-1);
-    Robot c = new Robot(5,10,East,-1);
-    Robot a = new Robot(1,15,East,-1);
-    Robot s = new Robot(1,20,East,-1);
     World.setVisible(true);
     World.setSize(6,23);
     World.setDelay(20);
+    try {
+      Thread.sleep(10000); // Pause for 1 second
+    } catch (InterruptedException e) {
+      // Handle the interruption
+    }
 
-
+    
     Thread lThread = new Thread(() -> {
-        l.putBeeper();
-        move(l, 4, true);
-        l.turnLeft();
-        move(l, 2, true);
+      Robot l = new Robot(5,1,South,-1);
+      l.putBeeper();
+      move(l, 4, true);
+      l.turnLeft();
+      move(l, 2, true);
     });
 
     Thread uThread = new Thread(() -> {
+      Robot u = new Robot(1,5,East,-1);
       u.turnLeft();
       u.putBeeper();
       move(u, 4, true);
@@ -39,55 +42,56 @@ public class Main implements Directions {
 
 
     Thread cThread = new Thread(() -> {
-    c.putBeeper();
-    move(c, 3, true);
+      Robot c = new Robot(5,10,East,-1);
+      c.putBeeper();
+      move(c, 3, true);
 
-    c.turnLeft();
-    c.turnLeft();
-    move(c, 3, false);
-    c.turnLeft();
-    
-    move(c, 4, true);
-    c.turnLeft();
-    move(c, 3, true);
+      c.turnLeft();
+      c.turnLeft();
+      move(c, 3, false);
+      c.turnLeft();
+      
+      move(c, 4, true);
+      c.turnLeft();
+      move(c, 3, true);
     });
 
 
     Thread aThread = new Thread(() -> {
-    a.putBeeper();
-    
-    a.turnLeft();
-    move(a, 3, true);
-    a.move();
-    turnRight(a);
-    a.move();
-    a.putBeeper();
-    a.move();
-    a.putBeeper();
-    a.move();
-    turnRight(a);
-    
-    move(a, 4, true);
-    a.turnLeft();
-    a.turnLeft();
-    move(a, 2, false);
-    a.turnLeft();
-    move(a, 2, true);
+      Robot a = new Robot(1,15,East,-1);
+      a.putBeeper();
+      
+      a.turnLeft();
+      move(a, 3, true);
+      a.move();
+      turnRight(a);
+      a.move();
+      a.putBeeper();
+      a.move();
+      a.putBeeper();
+      a.move();
+      turnRight(a);
+      
+      move(a, 4, true);
+      a.turnLeft();
+      a.turnLeft();
+      move(a, 2, false);
+      a.turnLeft();
+      move(a, 2, true);
     });
     
     Thread sThread = new Thread(() -> {
-    s.putBeeper();
-    move(s, 2, true);
-    s.turnLeft();
-    move(s, 2, true);
-    s.turnLeft();
-    move(s, 2, true);
-    turnRight(s);
-    move(s, 2, true);
-    turnRight(s);
-    move(s, 2, true);
-    
-    s.move();
+      Robot s = new Robot(1,20,East,-1);
+      s.putBeeper();
+      move(s, 2, true);
+      s.turnLeft();
+      move(s, 2, true);
+      s.turnLeft();
+      move(s, 2, true);
+      turnRight(s);
+      move(s, 2, true);
+      turnRight(s);
+      move(s, 2, true);
     });
 
 
@@ -96,10 +100,6 @@ public class Main implements Directions {
     cThread.start();
     aThread.start();
     sThread.start();
-    //a
-
-
-    //s
 
   }
 
