c90f9cf8c818e1639ab877f92f670f77a8559d0f
Lucas Moidel <lucas122133@gmail.com>
2025-08-19 09:08:37 -0700

    things

---
+++ dimond/Main.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.event.*;
+import kareltherobot.*;
+//This is the starter code for Karel the Robot AP CSA Thai
+
+public class Main implements Directions {
+
+
+  public static void turnRight(Robot fred) {
+    for (int i = 0; i < 3; i++) {
+      fred.turnLeft();
+    }
+  }
+
+  public static void turnLeft(Robot fred) {
+    fred.turnLeft();
+  }
+  public static void diag(Robot fred, Direction dir, int length) {
+    while (fred.direction() != dir) {
+      turnLeft(fred);
+    }
+    for (int x = 0; x < length; x++) {
+      fred.putBeeper();
+      fred.move();
+      turnLeft(fred);
+      fred.move();
+      turnRight(fred);
+    }
+  }
+  public static void main(String[] args) {
+    int length = 50;
+    Robot fred = new Robot(1, length+1, North, -1);
+    World.setVisible(true);
+    World.setSize(length*2+1, length*2+1);
+    World.setDelay(2);
+    
+    
+    diag(fred, North, length);
+    diag(fred, East, length);
+    diag(fred, South, length);
+    diag(fred, West, length);
+    }
+
+}
