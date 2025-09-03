a5aaf863b050c02e884b11beffb331ffcd9e6a2b
Lucas Moidel <lucas122133@gmail.com>
2025-09-03 09:16:33 -0700

    #3

--- sandbox7/src/Main.java
+++ sandbox7/src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args){
-    numOccur("happy", "a");
-    numOccur("happy", "p");
-    numOccur("happy", "A");
+    // numOccur("happy", "a");
+    // numOccur("happy", "p");
+    // numOccur("happy", "A");
 
     // #2: returns the number of occurrences of word, which may have many characters
-    System.out.println(numOccurs("happy to be with the two of you too", "to"));
-    System.out.println(numOccurs("happy to be with the two of you too", "th"));
-    System.out.println(numOccurs("happy to be with the two of you too", "o"));
-    System.out.println(numOccurs("happy to be with the two of you too", "Happy"));
+    // System.out.println(numOccurs("happy to be with the two of you too", "to"));
+    // System.out.println(numOccurs("happy to be with the two of you too", "th"));
+    // System.out.println(numOccurs("happy to be with the two of you too", "o"));
+    // System.out.println(numOccurs("happy to be with the two of you too", "Happy"));
 
     // #3: returns the index where the characters in the String begin to increase (the ASCII Value increase)
-    // ex.  findIncreaseStart("ZZYYYYYRMPLLCCA")  return 9 because that is the index of the first increasing char
-    // ex.  findIncreaseStart("ZZYYYYYRM")  return -1 because there is no increasing char
-    // ex.  findIncreaseStart("ABC")  return 1 because that is the index of the first increasing char
+    System.out.println(findIncreaseStart("ZZYYYYYRMPLLCCA"));
+    System.out.println(findIncreaseStart("ZZYYYYYRM"));
+    System.out.println(findIncreaseStart("ABC"));
     // In answering this, you MUST use compareTo
 
     // #4: returns the number of vowels in str.  In solving this problem, you MUST use numOccur.
@@ -50,9 +50,18 @@ public class Main {
 
 
     //#3
-    public int findIncreaseStart(String str){
-
-        return 0;
+    public static int findIncreaseStart(String str){
+        String letters = new String("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        int[] ints = new int[str.length()];
+        for (int i = 0; i<str.length(); i++){
+            ints[i] = letters.indexOf(str.substring(i, i+1));
+        }
+        for (int i = 1; i<ints.length; i++){
+            if (ints[i-1] < ints[i]){
+                return i;
+            }
+        }
+        return -1;
     }
 
     //#4
